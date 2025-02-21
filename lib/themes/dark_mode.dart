60b9f0dca3f632ccfbdc54c921662286ab8985e0
AnahatM <anahat.mudgal@gmail.com>
2025-02-21 15:35:17 -0500

    Theme provider

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade900,
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade800,
+    tertiary: Colors.grey.shade500,
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
