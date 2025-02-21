d24da279afeba0825ec60ac93bc808a9f365865e
AnahatM <anahat.mudgal@gmail.com>
2025-02-21 15:49:13 -0500

    Dark mode toggle

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:minimalist_chat/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -13,6 +16,38 @@ class SettingsPage extends StatelessWidget {
         title: Text(
           "Settings",
           style: TextStyle(color: Theme.of(context).colorScheme.primary),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        margin: const EdgeInsets.all(24),
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Dark Mode
+            Text(
+              "Dark Mode",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+            // Switch Toggle
+            CupertinoSwitch(
+              activeTrackColor: Theme.of(context).colorScheme.primary,
+              value: Provider.of<ThemeProvider>(context).isDarkMode,
+              onChanged:
+                  (value) =>
+                      Provider.of<ThemeProvider>(
+                        context,
+                        listen: false,
+                      ).toggleTheme(),
+            ),
+          ],
         ),
       ),
     );
